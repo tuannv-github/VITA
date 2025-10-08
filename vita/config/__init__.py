b431e6667a28f4beccd292efb5dcc3ccf5a95d92
Tuan V. Ngo <tuannv.email@gmail.com>
2025-10-08 17:02:03 +0000

    VITA model for go2 has been trained, the next step is to test the model with real hardware.

--- vita/config/__init__.py
+++ vita/config/__init__.py
@@ -1,9 +1,11 @@
 from .dataset_config import *
 
 NaturalCap = [LLaVADataset]
+GO2RobotDatasets = [GO2RobotDataset]
 
 DataConfig = {
     "llava-instruct-150k": NaturalCap,
+    "go2_robot": GO2RobotDatasets
 }
 
 NoPatchSets = ["khair", "jester"]
--- vita/config/dataset_config.py
+++ vita/config/dataset_config.py
@@ -5,3 +5,4 @@ FolderDict = {
 }
 
 LLaVADataset = {"chat_path": "/workspace/datasets/llava/adamo1139_llava-instruct-150k-with-images/llava_instruct_150k_newpath.json"}
+GO2RobotDataset = {"chat_path": "/workspace/verl/vita/go2_robot_dataset.json"}
